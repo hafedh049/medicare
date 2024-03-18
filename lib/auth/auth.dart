6cddcd9f06825bea5c67a09c7104cf326a238970
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-03-18 20:25:28 +0100

    Updates

---
+++ lib/auth/auth.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthInterface extends StatefulWidget {
+  const AuthInterface({super.key});
+
+  @override
+  State<AuthInterface> createState() => _AuthInterfaceState();
+}
+
+class _AuthInterfaceState extends State<AuthInterface> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
