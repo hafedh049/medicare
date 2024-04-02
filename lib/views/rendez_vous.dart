ef3a1b40e4ded294062049fbaa1237e8f35d237e
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-04-02 13:18:48 +0100

    Updates

--- lib/views/rendez_vous.dart
+++ lib/views/rendez_vous.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import '../utils/shared.dart';
 
@@ -30,6 +31,14 @@ class _RendezVousState extends State<RendezVous> {
           const SizedBox(height: 20),
           LottieBuilder.asset("assets/lotties/welcome.json", fit: BoxFit.contain, reverse: true),
           TextField(controller: _reasonController, decoration: const InputDecoration(labelText: "Reason", hintText: "Pour quoi")),
+          const SizedBox(height: 20),
+          Expanded(
+            child: TableCalendar(
+              focusedDay: DateTime.now(),
+              firstDay: DateTime(1960),
+              lastDay: DateTime(4000),
+            ),
+          ),
           const SizedBox(height: 20),
           Container(
             padding: const EdgeInsets.all(8),
